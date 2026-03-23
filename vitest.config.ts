676591dff58dce8576194d74cee1601bda2c67fc
Sam Richard <snugug@gmail.com>
2026-03-23 07:55:18 -0400

    Add Vitest configuration with c8 coverage

---
+++ vitest.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vitest/config';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+
+export default defineConfig({
+  plugins: [svelte()],
+  test: {
+    include: ['tests/**/*.test.ts'],
+    coverage: {
+      provider: 'v8',
+      enabled: true,
+    },
+  },
+});
