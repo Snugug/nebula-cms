af858fa13099f767af4dc07e9af60fdafc8776f6
Sam Richard <snugug@gmail.com>
2026-03-23 07:56:29 -0400

    Add source file stubs for astro integration, client, and types

---
+++ src/types.ts
@@ -0,0 +1,7 @@
+/**
+ * Placeholder configuration for nebula-cms.
+ * Will be expanded as the CMS feature set is defined.
+ */
+export interface NebulaCMSConfig {
+  // Configuration options will be added here
+}
