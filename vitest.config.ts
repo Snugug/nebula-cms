e092acfb80d9a10a5dc4a05b5f7cb30008e2075f
Sam Richard <snugug@gmail.com>
2026-04-04 06:20:15 -0400

    feat: display code coverage table after test runs

--- vitest.config.ts
+++ vitest.config.ts
@@ -48,6 +48,7 @@ export default defineConfig({
           coverage: {
             provider: 'v8',
             reportsDirectory: '.coverage',
+            reporter: ['text', 'json'],
           },
         },
       },
